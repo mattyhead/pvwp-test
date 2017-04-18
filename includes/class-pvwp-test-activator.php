5159f5446ba0445f2d7df0a1dcc739fabd6cd388
matt murphy <mattyhead@gmail.com>
2017-04-18 12:04:11 -0400

    first commit

---
+++ includes/class-pvwp-test-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       github.com/mattyhead
+ * @since      1.0.0
+ *
+ * @package    Pvwp_Test
+ * @subpackage Pvwp_Test/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Pvwp_Test
+ * @subpackage Pvwp_Test/includes
+ * @author     Matthew Murphy <matthew.e.murphy@phila.gov>
+ */
+class Pvwp_Test_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-pvwp-test-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       github.com/mattyhead
+ * @since      1.0.0
+ *
+ * @package    Pvwp_Test
+ * @subpackage Pvwp_Test/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Pvwp_Test
+ * @subpackage Pvwp_Test/includes
+ * @author     Matthew Murphy <matthew.e.murphy@phila.gov>
+ */
+class Pvwp_Test_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
